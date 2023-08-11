979e94b82459f03e97270b0903511fed88c6a094
Xcutor <Lekanabass50@gmail.com>
2023-08-11 15:56:58 -0700

    all tasks done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int m,n;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 48; m < 58; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int j,n;
+
+	for (n = 0; n < 100; n++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (n < j)
+			{
+				putchar((n/10)+48);
+				putchar((n%10)+48);
+				putchar(' ');
+				putchar((j/10)+48);
+				putchar((j%10)+48);
+				if (n != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
