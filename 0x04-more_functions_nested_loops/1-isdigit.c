cf168c08f95c151d4825d90a72176c23939a16af
Xcutor <Lekanabass50@gmail.com>
2023-08-17 12:20:31 -0700

    task 2

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,11 +2,12 @@
 /**
  * _isdigit - checks for digits
  * @c: digit to be tested
+ *
  * Return: returns 1 for digits and 0 for non digits
  */
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 58)
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
