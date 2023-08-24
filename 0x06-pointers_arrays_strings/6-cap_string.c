fba17c8cf90013c91d3e8da49fb586f6dfa060d5
XcutorALX <Lekanabass50@gmail.com>
2023-08-24 10:47:50 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * cap_string - a function that capitalizes all words of a string
+ * @str: the string to be changed
+ *
+ * Return: returns the pointer to the converted string
+ */
+char *cap_string(char *str)
+{
+	int i, newWord;
+
+	newWord = 1;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (newWord == 1)
+		{
+			if (str[i] >= 'a' &&
+				str[i] <= 'z')
+			{
+				newWord = 0;
+				str[i] -= 32;
+			}
+			else if (str[i] >= 'A' &&
+				str[i] <= 'Z')
+			{
+				newWord = 0;
+			}
+		}
+		else if (str[i] >= 'A' &&
+			str[i] <= 'Z' &&
+			newWord == 0)
+			str[i] += 32;
+		else if (str[i] == ' ' ||
+			str[i] == '\t' ||
+			str[i] == '\n' ||
+			str[i] == ',' ||
+			str[i] == ';' ||
+			str[i] == '.' ||
+			str[i] == '!' ||
+			str[i] == '?' ||
+			str[i] == '"' ||
+			str[i] == '(' ||
+			str[i] == ')' ||
+			str[i] == '{' ||
+			str[i] == '}')
+			newWord = 1;
+	}
+	return (str);
+}
