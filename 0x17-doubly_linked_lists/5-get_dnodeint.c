1f26bfd3bf4beff45f984d6e88f8e937ad0d267c
Xcutor <Lekanabass50@gmail.com>
2024-01-11 22:08:41 -0600

    task 8 done

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		return (NULL);
 
 	currentNode = head;
-	for (current = 0; currentNode->next != NULL; current++)
+	for (current = 0; currentNode != NULL; current++)
 	{
 		if (current == index)
 			return (currentNode);
