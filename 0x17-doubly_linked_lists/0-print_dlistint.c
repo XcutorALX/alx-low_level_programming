bbbb78b6665707edf756fecc048a5939ed20f12a
Xcutor <Lekanabass50@gmail.com>
2024-01-11 20:05:06 -0600

    task done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -16,7 +16,7 @@ size_t print_dlistint(const dlistint_t *h)
 	dlistint_t *current;
 
 	if (h == NULL)
-		return (0);	
+		return (0);
 
 	current = h->next->prev;
 	for (nodes = 0; current != NULL; nodes++)
